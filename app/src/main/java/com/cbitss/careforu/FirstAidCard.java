a97beef2f485b806fbf010efab21a2c7f7fe1bc3
Lenovo <anshit.singh18@gmail.com>
2020-06-08 11:40:59 +0530

    A complete health care app with elegant GUI and following features : customized Medicine Reminder, daily health tips, information about nearby hospitals with navigation, complete information regarding a medicine or salt and first aid tips. First aid tips are fetched from remote database with 'save offline' feature for emergency.

---
+++ app/src/main/java/com/cbitss/careforu/FirstAidCard.java
@@ -0,0 +1,22 @@
+package com.cbitss.careforu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FirstAidCard {
+
+    int id;
+    String title,content;
+    String imageId;
+
+    FirstAidCard(int id,String title,String content,String imageId)
+    {
+        this.title = title;
+        this.id=id;
+        this.content = content;
+        this.imageId = imageId;
+    }
+
+
+}
